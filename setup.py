d72d5410a724286910c900596f9acb305f5eb438
Max Tyler <maxastyler@gmail.com>
2021-03-08 12:06:53 +0000

    Add skeleton of python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rusty-slm',
+    version='1.0.0',
+    url='https://github.com/maxastyler/rusty-slm',
+    author='Max Tyler',
+    author_email='maxastyler@gmail.com',
+    description='Functions to interact with an SLM',
+    packages=find_packages(include=["slmmm"]),
+    install_requires=["grpcio>=1.35", "grpcio-tools>=1.35"]
+)
