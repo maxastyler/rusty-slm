6d15e7dc303036c6fe45b66dadf120860a986a23
Max Tyler <maxastyler@gmail.com>
2021-11-11 23:14:22 +0000

    Update rust channel

--- shell.nix
+++ shell.nix
@@ -1,12 +1,13 @@
 let
   moz_overlay = import (builtins.fetchTarball
-    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+    "https://github.com/maxastyler/nixpkgs-mozilla/archive/master.tar.gz");
 in { pkgs ? import <nixpkgs> { overlays = [ moz_overlay ]; } }:
 pkgs.mkShell rec {
   buildInputs = [
-    pkgs.hello
+    # keep this line if you use bash
+    pkgs.bashInteractive
     ((pkgs.rustChannelOf {
-      date = "2021-02-28";
+      date = "2021-06-10";
       channel = "nightly";
     }).rust.override {
       extensions = [ "rust-src" "rustfmt-preview" "clippy-preview" ];
@@ -14,7 +15,6 @@ pkgs.mkShell rec {
     pkgs.rust-analyzer
     pkgs.nixfmt
     # keep this line if you use bash
-    pkgs.bashInteractive
     pkgs.pkgconfig
     pkgs.xorg.libX11
     pkgs.xorg.libXcursor
@@ -29,5 +29,5 @@ pkgs.mkShell rec {
         grpcio-tools
       ])).override (args: { ignoreCollisions = true; }))
   ];
-  LD_LIBRARY_PATH = with pkgs; stdenv.lib.makeLibraryPath buildInputs;
+  LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath buildInputs;
 }
