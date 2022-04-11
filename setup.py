cb91af7efec849927b3c75cb61532cc0171e2b2d
Max Tyler <maxastyler@gmail.com>
2022-04-11 16:07:44 +0100

    Update grpcio version for natalia (hope u enjoy)

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='rusty_slm',
-    version='1.0.0',
-    url='https://github.com/maxastyler/rusty-slm',
-    author='Max Tyler',
-    author_email='maxastyler@gmail.com',
-    description='Functions to interact with an SLM',
+    name="rusty_slm",
+    version="1.0.0",
+    url="https://github.com/maxastyler/rusty-slm",
+    author="Max Tyler",
+    author_email="maxastyler@gmail.com",
+    description="Functions to interact with an SLM",
     packages=find_packages(include=["rusty_slm"]),
-    install_requires=["grpcio>=1.35", "grpcio-tools>=1.35"]
+    install_requires=["grpcio==1.32", "grpcio-tools==1.32"],
 )
