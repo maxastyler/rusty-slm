61263b0ce725ee7fc2b627ff9b4851f919dace02
maxastyler <maxastyler@gmail.com>
2023-01-16 18:22:38 +0000

    Update rusty slm

--- rusty_slm/slm_pb2_grpc.py
+++ rusty_slm/slm_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from rusty_slm import slm_pb2 as slm__pb2
+import slm_pb2 as slm__pb2
 
 
 class SLMStub(object):
